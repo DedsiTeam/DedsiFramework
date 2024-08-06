41cd6d9d20be825d8ec538c57aa5b1128aec55f4
Cohen <1768065921@qq.com>
2024-08-06 20:57:58 +0800

    EntityFrameworkCore
    EntityFrameworkCore code

---
+++ templates/cleanArchitecture/src/ProjectName.Infrastructure/EntityFrameworkCore/ProjectNameDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace ProjectName.Infrastructure.EntityFrameworkCore;
+
+public class ProjectNameDbContext(DbContextOptions<ProjectNameDbContext> options)
+    : AbpDbContext<ProjectNameDbContext>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
+}
--- templates/cleanArchitecture/src/ProjectName.Infrastructure/ProjectNameInfrastructureModule.cs
+++ templates/cleanArchitecture/src/ProjectName.Infrastructure/ProjectNameInfrastructureModule.cs
@@ -1,8 +1,19 @@
-﻿using Volo.Abp.Modularity;
+﻿using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.Modularity;
 
 namespace ProjectName.Infrastructure;
 
+[DependsOn(
+    typeof(AbpEntityFrameworkCoreModule)
+)]
 public class ProjectNameInfrastructureModule : AbpModule
 {
-    
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            // context.Services.AddAbpDbContext<SystemManagementDbContext>(options =>
+            // {
+            //     options.AddDefaultRepositories(true);
+            // });
+        }
+
 }
