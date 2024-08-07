398cb0cd748988e05fdaabf46a3566bb2888545d
Cohen <1768065921@qq.com>
2024-08-07 21:15:27 +0800

    添加日志

--- templates/cleanArchitecture/host/ProjectName.HttpApi.Host/Program.cs
+++ templates/cleanArchitecture/host/ProjectName.HttpApi.Host/Program.cs
@@ -1,12 +1,47 @@
-using ProjectName;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace ProjectName;
 
-builder.Host.UseAutofac();
-
-await builder.AddApplicationAsync<ProjectNameHttpApiHostModule>();
-
-var app = builder.Build();
-
-await app.InitializeApplicationAsync();
-await app.RunAsync();
+public class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        try
+        {
+            Log.Information("ProjectName web host.");
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Host
+                .AddAppSettingsSecretsJson()
+                .UseAutofac()
+                .UseSerilog((context, services, loggerConfiguration) =>
+                {
+                    loggerConfiguration
+                        #if DEBUG
+                        .MinimumLevel.Debug()
+                        #else
+                        .MinimumLevel.Information()
+                        #endif
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                        .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+                        .Enrich.FromLogContext()
+                        .WriteTo.Async(c => c.File(path:"Logs/logs.txt", rollingInterval:RollingInterval.Hour, retainedFileCountLimit: null))
+                        .WriteTo.Async(c => c.Console());
+                });
+            await builder.AddApplicationAsync<ProjectNameHttpApiHostModule>();
+            var app = builder.Build();
+            await app.InitializeApplicationAsync();
+            await app.RunAsync();
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "ProjectName Host terminated unexpectedly!");
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
