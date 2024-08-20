0d557c3b38b6cf0df049f71d34ef847e7ed18069
Cohen <1768065921@qq.com>
2024-08-20 08:03:36 +0800

    完善
    完善

--- templates/cleanArchitectureCQRS/content/src/ProjectNameCQRS.Infrastructure/ProjectNameCQRSInfrastructureModule.cs
+++ templates/cleanArchitectureCQRS/content/src/ProjectNameCQRS.Infrastructure/ProjectNameCQRSInfrastructureModule.cs
@@ -1,5 +1,6 @@
 ﻿using Dedsi.CleanArchitecture.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
+using ProjectNameCQRS.Emails;
 using ProjectNameCQRS.EntityFrameworkCore;
 using Volo.Abp.Modularity;
 
@@ -12,9 +13,13 @@ public class ProjectNameCQRSInfrastructureModule : AbpModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
+        // EntityFrameworkCore
         context.Services.AddAbpDbContext<ProjectNameCQRSDbContext>(options =>
         {
             options.AddDefaultRepositories(true);
         });
+
+        // 测试发送邮件
+        context.Services.AddTransient<IEmailSender, TestEmailSender>();
     }
 }
