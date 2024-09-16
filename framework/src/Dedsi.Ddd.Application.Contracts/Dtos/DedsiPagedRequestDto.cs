811d7cf757e2d7a9e019a025f8f005ffc233b898
Cohen <1768065921@qq.com>
2024-09-16 21:07:15 +0800

    Update DedsiPagedRequestDto.cs

--- framework/src/Dedsi.Ddd.Application.Contracts/Dtos/DedsiPagedRequestDto.cs
+++ framework/src/Dedsi.Ddd.Application.Contracts/Dtos/DedsiPagedRequestDto.cs
@@ -7,5 +7,24 @@ namespace Dedsi.Ddd.Application.Contracts.Dtos;
 /// </summary>
 public class DedsiPagedRequestDto : PagedResultRequestDto
 {
+    private int _pageIndex = 1;
+
+    private int _pageSize = 10;
+
+    public virtual int PageIndex 
+    { 
+        get => _pageIndex; 
+        set => _pageIndex = value <= 0 ? 1 : value; 
+    }
+
+    public virtual int PageSize 
+    { 
+        get => _pageSize; 
+        set => _pageSize = value > 1000 ? 10 : value <= 0 ? 10 : value; 
+    }
+
+    public override int MaxResultCount => PageSize;
+
+    public override int SkipCount => (PageSize - 1) * PageSize;
 }
 
