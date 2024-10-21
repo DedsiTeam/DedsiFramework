ec0123c08656fd9923194709a24625e1ddf8f0db
Cohen <1768065921@qq.com>
2024-10-21 11:48:34 +0800

    8.3.2
    8.3.2

--- framework/src/Dedsi.Ddd.Application.Contracts/DedsiDddApplicationContractsModule.cs
+++ framework/src/Dedsi.Ddd.Application.Contracts/DedsiDddApplicationContractsModule.cs
@@ -1,9 +1,11 @@
 ﻿using Volo.Abp.Application;
+using Volo.Abp.FluentValidation;
 using Volo.Abp.Modularity;
 
 namespace Dedsi.Ddd.Application.Contracts;
 
 [DependsOn(
+    typeof(AbpFluentValidationModule),
     typeof(AbpDddApplicationContractsModule)
 )]
 public class DedsiDddApplicationContractsModule : AbpModule
--- framework/src/Dedsi.Ddd.CQRS/CommandHandlers/IDedsiCommandHandler.cs
+++ framework/src/Dedsi.Ddd.CQRS/CommandHandlers/IDedsiCommandHandler.cs
@@ -1,11 +1,12 @@
 using Dedsi.Ddd.CQRS.Commands;
 using MediatR;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.Validation;
 
 namespace Dedsi.Ddd.CQRS.CommandHandlers;
 
 
-public interface IDedsiCommandHandler : ITransientDependency;
+public interface IDedsiCommandHandler : ITransientDependency, IValidationEnabled;
 
 public interface IDedsiCommandHandler<in TCommand> : IDedsiCommandHandler, IRequestHandler<TCommand> where TCommand : IDedsiCommand;
 
