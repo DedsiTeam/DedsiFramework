7698a43a78b9d002d1175d31d8ca3ea0f80a06cd
Cohen <1768065921@qq.com>
2025-01-12 15:56:05 +0800

    调整

--- framework/src/Dedsi.Ddd.Domain.Shared/EntityIds/EntityId.cs
+++ framework/src/Dedsi.Ddd.Domain.Shared/EntityIds/EntityId.cs
@@ -6,19 +6,29 @@ public class EntityId : IEntityId;
 
 public record StronglyTypedId<TSource> : IStronglyTypedId<TSource>
 {
+    public StronglyTypedId(TSource id)
+    {
+        Id = id;
+    }
+
     public virtual TSource Id { get; }
+
+    public override string ToString()
+    {
+        return $"Id = {Id}.";
+    }
 }
 
-public record Int64StronglyTypedId(long Id) : StronglyTypedId<Int64>, IInt64StronglyTypedId
+public record Int64StronglyTypedId(long Id) : StronglyTypedId<Int64>(Id), IInt64StronglyTypedId
 {
     public Int64StronglyTypedId() : this(YitIdHelper.NextId())
     {
     }
 }
 
-public record GuidStronglyTypedId(Guid Id) : StronglyTypedId<Guid>, IGuidStronglyTypedId;
+public record GuidStronglyTypedId(Guid Id) : StronglyTypedId<Guid>(Id), IGuidStronglyTypedId;
 
-public record UlidStronglyTypedId(Ulid Id) : StronglyTypedId<Ulid>, IUlidStronglyTypedId
+public record UlidStronglyTypedId(Ulid Id) : StronglyTypedId<Ulid>(Id), IUlidStronglyTypedId
 {
     public UlidStronglyTypedId() : this(Ulid.NewUlid())
     {
