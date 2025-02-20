a9f44a16e2894e77ec1aeb105d61939294f6e366
Cohen <1768065921@qq.com>
2025-02-20 17:29:57 +0800

    调整DTO的位置

--- templates/cleanArchitectureCQRS/content/src/ProjectNameCQRS.MinimalApi/Users/Dtos/CreateUserInputDto.cs
+++ templates/cleanArchitectureCQRS/content/src/ProjectNameCQRS.MinimalApi/Users/Dtos/CreateUserInputDto.cs
@@ -1,14 +1,14 @@
-using Volo.Abp.Application.Dtos;
-
 namespace ProjectNameCQRS.Users.Dtos;
 
-public class UserDto : EntityDto<Guid>
+public class CreateUserInputDto
 {
     public string UserName { get; set; }
 
     public string Account { get; set; }
 
-    public string Password { get; set; }
+    public string PassWord { get; set; }
 
     public string Email { get; set; }
 }
+
+public class UpdateUserInputDto : UserDto;
--- templates/cleanArchitectureCQRS/content/src/ProjectNameCQRS.UseCase/Users/Dtos/LoginInputDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProjectNameCQRS.Users.Dtos;
-
-public class LoginInputDto
-{
-    public string Account { get; set; }
-
-    public string PassWord { get; set; }
-}
