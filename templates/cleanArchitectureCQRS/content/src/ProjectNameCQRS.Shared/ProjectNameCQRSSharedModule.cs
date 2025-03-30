b7a55aa06134aba332a63d1acdaf1fe66233c144
Cohen <1768065921@qq.com>
2025-03-30 18:23:40 +0800

    add DedsiDddApplicationContractsModule

--- templates/cleanArchitectureCQRS/content/src/ProjectNameCQRS.Shared/ProjectNameCQRSSharedModule.cs
+++ templates/cleanArchitectureCQRS/content/src/ProjectNameCQRS.Shared/ProjectNameCQRSSharedModule.cs
@@ -1,9 +1,11 @@
-﻿using Dedsi.Ddd.Domain.Shared;
+﻿using Dedsi.Ddd.Application.Contracts;
+using Dedsi.Ddd.Domain.Shared;
 using Volo.Abp.Modularity;
 
 namespace ProjectNameCQRS;
 
 [DependsOn(
+    typeof(DedsiDddApplicationContractsModule),
     typeof(DedsiDddDomainSharedModule)    
 )]
 public class ProjectNameCQRSSharedModule: AbpModule;
