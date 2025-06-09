4a7cd733180447facdc924c75e9ebdc772403a71
Cohen <1768065921@qq.com>
2025-06-09 11:49:25 +0800

    添加代码

---
+++ modules/DedsiPermission/src/DedsiPermission.UseCase/Permissions/CommandHandlers/DeletePermissionCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Dedsi.Ddd.CQRS.CommandHandlers;
+using Dedsi.Ddd.CQRS.Commands;
+using DedsiPermission.Repositories.Permissions;
+
+namespace DedsiPermission.Permissions.CommandHandlers;
+
+/// <summary>
+/// 删除权限命令
+/// </summary>
+/// <param name="PermissionId">权限ID</param>
+public record DeletePermissionCommand(string PermissionId) : DedsiCommand<bool>;
+
+/// <summary>
+/// 删除权限命令处理器
+/// </summary>
+/// <param name="permissionRepository">权限仓储</param>
+public class DeletePermissionCommandHandler(IPermissionRepository permissionRepository) : DedsiCommandHandler<DeletePermissionCommand, bool>
+{
+    public override async Task<bool> Handle(DeletePermissionCommand command, CancellationToken cancellationToken)
+    {
+        var permission = await permissionRepository.GetAsync(a => a.Id == command.PermissionId, true, cancellationToken);
+
+        await permissionRepository.DeleteAsync(permission, false, cancellationToken);
+
+        return true;
+    }
+}
---
+++ modules/DedsiPermission/src/DedsiPermission.UseCase/Permissions/CommandHandlers/UpdatePermissionCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using Dedsi.Ddd.CQRS.CommandHandlers;
+using Dedsi.Ddd.CQRS.Commands;
+using DedsiPermission.Repositories.Permissions;
+
+namespace DedsiPermission.Permissions.CommandHandlers;
+
+/// <summary>
+/// 更新权限命令
+/// </summary>
+/// <param name="Id">权限ID</param>
+/// <param name="PermissionName">权限名称</param>
+/// <param name="PermissionDescription">权限描述</param>
+/// <param name="PermissionType">权限类型</param>
+/// <param name="PermissionGroupCode">权限组代码</param>
+/// <param name="PermissionGroupName">权限组名称</param>
+/// <param name="ParentId">父权限ID</param>
+public record UpdatePermissionCommand(
+    string Id,
+    string PermissionName,
+    string PermissionDescription,
+    PermissionType PermissionType,
+    string PermissionGroupCode,
+    string PermissionGroupName,
+    string? ParentId = null) : DedsiCommand<bool>;
+
+/// <summary>
+/// 更新权限命令处理器
+/// </summary>
+/// <param name="permissionRepository">权限仓储</param>
+public class UpdatePermissionCommandHandler(IPermissionRepository permissionRepository) : DedsiCommandHandler<UpdatePermissionCommand, bool>
+{
+    public override async Task<bool> Handle(UpdatePermissionCommand command, CancellationToken cancellationToken)
+    {
+        var permission = await permissionRepository.GetAsync(a => a.Id == command.Id, true, cancellationToken);
+        if (permission == null)
+        {
+            return false;
+        }
+
+        permission.ChangePermissionName(command.PermissionName);
+        permission.ChangePermissionDescription(command.PermissionDescription);
+        permission.ChangePermissionType(command.PermissionType);
+        permission.ChangePermissionGroupCode(command.PermissionGroupCode);
+        permission.ChangePermissionGroupName(command.PermissionGroupName);
+        permission.ChangeParentId(command.ParentId);
+
+        await permissionRepository.UpdateAsync(permission, false, cancellationToken);
+
+        return true;
+    }
+}
