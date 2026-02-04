b33f4ec6a50d433e39d9683a15fc11bc3bf2d4eb
Cohen <1768065921@qq.com>
2026-02-04 11:33:01 +0800

    1

--- src/Dedsi.CleanArchitecture.HttpApi/DedsiCleanArchitectureHttpApiModule.cs
+++ src/Dedsi.CleanArchitecture.HttpApi/DedsiCleanArchitectureHttpApiModule.cs
@@ -1,11 +1,9 @@
 ﻿using Dedsi.AspNetCore;
-using Volo.Abp.AspNetCore.Mvc;
 using Volo.Abp.Modularity;
 
 namespace Dedsi.CleanArchitecture.HttpApi;
 
 [DependsOn(
-    typeof(AbpAspNetCoreMvcModule),
     typeof(DedsiAspNetCoreModule)
 )]
 public class DedsiCleanArchitectureHttpApiModule : AbpModule;
